12848b136e01d8c58eac5a889ee5847cde62ed89
django <jannuraidynuly@gmail.com>
2021-01-19 14:30:54 +0600

    task interface language

---
+++ test_items.py
@@ -0,0 +1,8 @@
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    time.sleep(3)
+    btn = browser.find_elements_by_css_selector("[type='submite']")
+    assert btn, "Should be a button"
